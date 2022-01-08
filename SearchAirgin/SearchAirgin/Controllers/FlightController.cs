abd6c7d3032f975388b6673faed2cd47753f6729
Joknaa <54961980+Joknaa@users.noreply.github.com>
2022-01-08 13:42:25 +0100

    Refactored code + formatted the date and time display
    => Refactored and optimized the code
    => Reversed the flights order in the respond request (now it shows the near flights first)
    => Used Moment.js library to format the date and time that is displayed when searching.
    
    Next:
    => Make the user enter the actual date, then convert it to epoch internally.
    => Maybe add a database of airports for auto completion in search.

--- SearchAirgin/SearchAirgin/Controllers/FlightController.cs
+++ SearchAirgin/SearchAirgin/Controllers/FlightController.cs
@@ -48,86 +48,57 @@ public class FlightController : Controller {
         const long maxRequestInterval = 600000; // 6 days and 22h in Unix.
         long arrivalTime = departureTime + maxRequestInterval;
 
-        //var departingFlights = await GetFlights_FromDeparture(departure, departureTime, arrivalTime);
-        //Console.WriteLine("Got the GetFlights_FromDeparture");
-        //var arrivingFlights = await GetFlights_ToDestination(destination, departureTime, arrivalTime);
-        //Console.WriteLine("Got the GetFlights_ToDestination");
-        //var searchResult = FilterFlights(departingFlights, arrivingFlights);
-        
-        
-        List<Flight>? Flights_Dep;
-        List<Flight>? Flights_Des;
-        string request_1_URI =
-            $"https://opensky-network.org/api/flights/departure?airport={departure}&begin={departureTime}&end={arrivalTime}";
-        string request_2_URI =
-            $"https://opensky-network.org/api/flights/arrival?airport={destination}&begin={departureTime}&end={arrivalTime}";
+        List<Flight>? Flights_Dep, Flights_Des;
 
         using (var httpClient = new HttpClient(_clientHandler)) {
-            using (var response = await httpClient.GetAsync(request_1_URI)) {
+            using (var response = await httpClient.GetAsync(
+                       $"https://opensky-network.org/api/flights/departure?airport={departure}&begin={departureTime}&end={arrivalTime}"
+                   )) {
                 var apiResponse = await response.Content.ReadAsStringAsync();
                 Flights_Dep = JsonConvert.DeserializeObject<List<Flight>>(apiResponse);
             }
-            using (var response = await httpClient.GetAsync(request_2_URI)) {
+
+            using (var response = await httpClient.GetAsync(
+                       $"https://opensky-network.org/api/flights/arrival?airport={destination}&begin={departureTime}&end={arrivalTime}"
+                   )) {
                 var apiResponse = await response.Content.ReadAsStringAsync();
                 Flights_Des = JsonConvert.DeserializeObject<List<Flight>>(apiResponse);
             }
         }
-        
-        
+
         var searchResult = FilterFlights(Flights_Dep, Flights_Des, departure, destination);
-        
+
+        if (searchResult.Count == 0) {
+            Console.Write("nothing");
+        }
+
         return searchResult;
     }
-    
-    private async Task<List<Flight>?> GetFlights_FromDeparture(string departure, long departureTime, long arrivalTime) {
-        List<Flight>? _oFlights;
-        string requestURI =
-            $"https://opensky-network.org/api/flights/departure?airport={departure}&begin={departureTime}&end={arrivalTime}";
 
-        using (var httpClient = new HttpClient(_clientHandler)) {
-            using (var response = await httpClient.GetAsync(requestURI)) {
-                var apiResponse = await response.Content.ReadAsStringAsync();
-                _oFlights = JsonConvert.DeserializeObject<List<Flight>>(apiResponse);
-            }
-            httpClient.Dispose();
-        }
-
-        return _oFlights;
-    }
-    private async Task<List<Flight>?> GetFlights_ToDestination(string destination, long departureTime, long arrivalTime) {
-        List<Flight>? Flights;
-        string requestURI =
-            $"https://opensky-network.org/api/flights/arrival?airport={destination}&begin={departureTime}&end={arrivalTime}";
-
-        using (var httpClient = new HttpClient(_clientHandler)) {
-            using (var response = await httpClient.GetAsync(requestURI)) {
-                var apiResponse = await response.Content.ReadAsStringAsync();
-                Flights = JsonConvert.DeserializeObject<List<Flight>>(apiResponse);
-            }
-            httpClient.Dispose();
-        }
-
-        return Flights;
-    }
-
-    private static List<Flight> FilterFlights(List<Flight> departingFlights, List<Flight> arrivingFlights, string departure, string destination) {
+    private static List<Flight> FilterFlights(
+        List<Flight>? departingFlights,
+        List<Flight>? arrivingFlights,
+        string departure,
+        string destination
+    ) {
         List<Flight> Flights = new List<Flight>();
-        
+
         //string destination = arrivingFlights[0].estArrivalAirport;
         //string departure = departingFlights[0].estDepartureAirport;
 
-        departingFlights.ForEach(flight => {
+        departingFlights?.ForEach(flight => {
             if (flight.estArrivalAirport.Equals(destination)) {
                 Flights.Add(flight);
             }
         });
-        
-        arrivingFlights.ForEach(flight => {
+
+        arrivingFlights?.ForEach(flight => {
             if (flight.estDepartureAirport.Equals(departure)) {
                 Flights.Add(flight);
             }
         });
-        
+
+        Flights.Reverse();
         return Flights;
     }
 }
